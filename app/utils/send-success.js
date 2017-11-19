ae91be5a7dfce2047cccf591dee86a27f987b96c
Christian Abdelmassih <chrabd@kth.se>
2017-11-19 23:14:34 +0100

    add ajax, css-animations

--- app/utils/send-success.js
+++ app/utils/send-success.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = function(res) {
-  res.status(200).send('success');
+  res.status(200).json({});
 }
--- assets/scripts/subscribe.js
+++ assets/scripts/subscribe.js
@@ -1,7 +1,31 @@
-module.exports = function() {
-  var form = document.getElementById("subscribe-form");
+var $ = require('jquery');
+var swal = require('sweetalert');
 
-  document.getElementById("subscribe").addEventListener("click", function () {
-    form.submit();
+module.exports = function() {
+  var input = $("#subscribe-email");
+  var button = $("#subscribe");
+
+  button.click(function() {
+    var email = input.val();
+    if (email.indexOf('@') === -1) { return; }
+    $.post("/subscribe/", { email: email }, function(data, status){
+      if (status === 'success') {
+        swal({
+          title: "Success!",
+          text: "You are now subscribed and will recieve updates 😃",
+          icon: "success",
+          buttons: false,
+          timer: 3000,
+        });
+      } else {
+        swal({
+          title: "Oh noes!",
+          text: "Something, somewhere went wrong ",
+          icon: "error",
+          buttons: false,
+          timer: 3000,
+        });
+      }
+    });
   });
 }
